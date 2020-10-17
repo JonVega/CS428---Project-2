d4058df1ddbbce964240f39362274f67c1933fea
Jonathan Vega <jon.e.vega@icloud.com>
2020-10-17 12:31:15 -0500

    basic set done

--- Assets/Scripts/buttonAction.cs
+++ Assets/Scripts/buttonAction.cs
@@ -7,25 +7,29 @@ public class buttonAction : MonoBehaviour
 {
 	public GameObject vbBtnObj;
 	public AudioSource voiceObject;
+	public Animator anim;
 	
 	
     // Start is called before the first frame update
     void Start()
     {
+		//anim = GetComponent<Animator>();
 		vbBtnObj = GameObject.Find("button");
 		voiceObject = GetComponent<AudioSource>(); vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
         Debug.Log("start");
-		
     }
 	
 	public void OnButtonPressed(VirtualButtonBehaviour vb)
 	{
 	         Debug.Log("Button pressed");
-			 voiceObject.Play();
+			 anim.SetBool("isYelling", true);
+			 
 	}
 	public void OnButtonReleased(VirtualButtonBehaviour vb)
 	{
 	         Debug.Log("Button released");
+			 voiceObject.Play();
+			 //anim.SetBool("isMad", false);
 	}
 
     // Update is called once per frame
